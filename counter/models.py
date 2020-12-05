8414eb9bef50cd69331f59ff4049641c0d4ab933
markgardie <markgardie@users.noreply.github.com>
2020-12-05 22:40:41 +0200

    made models for counter

--- counter/models.py
+++ counter/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Breakfast(models.Model):
+    product_name = models.CharField(max_length=30)
+
+    def __srt__(self):
+        return self.name
+
+class Lunch(models.Model):
+    product_name = models.CharField(max_length=30)
+
+    def __srt__(self):
+        return self.name
+
+class Dinner(models.Model):
+    product_name = models.CharField(max_length=30)
+
+    def __srt__(self):
+        return self.name
--- counter/urls.py
+++ counter/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = "counter-index")
+    path('', views.index, name = "counter_index")
 ]
