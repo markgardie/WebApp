54321d21651dbeb95abaca35a02f83d146475461
markgardie <markgardie@users.noreply.github.com>
2020-12-06 22:40:55 +0200

    auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path, include
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.login, name = "login"),
+    path('', include('django.contrib.auth.urls')),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render, redirect
 import requests
 from django.urls import reverse_lazy
-
-def login(request):
-    return render(request, 'accounts/login.html')
