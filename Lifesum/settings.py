992a51674b415b1b3b4e5a74f461fd3616cfb2af
markgardie <markgardie@users.noreply.github.com>
2020-12-07 09:24:55 +0200

    Update settings.py

--- Lifesum/settings.py
+++ Lifesum/settings.py
@@ -131,3 +131,19 @@ LOGIN_REDIRECT_URL = '/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
